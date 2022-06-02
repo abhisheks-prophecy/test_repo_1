b510b48163c3a1522efd4cc6cb95fbe194ac78c3
abhisheks+e2etests@simpledatalabs.com <abhisheks+e2etests@simpledatalabs.com>
2022-06-02 22:16:36 +0000

    simple commit

---
+++ uitesting/e2etestsmainforked2/pipelines/Workflow-Automated-GITHUB-FORK-1654207669943/code/job/graph/Source_1.py
@@ -0,0 +1,18 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Source_1(spark: SparkSession) -> DataFrame:
+    if Config.fabricName == "dev":
+        return spark.read\
+            .format("json")\
+            .schema(
+              StructType([
+                StructField("account_flags", StringType(), True), StructField("account_open_date", StringType(), True), StructField("country_code", StringType(), True), StructField("customer_id", StringType(), True), StructField("email", StringType(), True), StructField("first_name", StringType(), True), StructField("last_name", StringType(), True), StructField("phone", StringType(), True)
+            ])
+            )\
+            .load("dbfs:/Prophecy/qa_data/json/CustomersDatasetInput.json")
+    else:
+        raise Exception("No valid dataset present to read fabric")
