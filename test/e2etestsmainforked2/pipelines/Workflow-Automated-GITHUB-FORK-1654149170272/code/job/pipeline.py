2904825aba177e4c50ca2abe44d67f5423cbb93d
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-06-02 05:54:12 +0000

    simple commit

--- test/e2etestsmainforked2/pipelines/Workflow-Automated-GITHUB-FORK-1654149170272/code/job/pipeline.py
+++ test/e2etestsmainforked2/pipelines/Workflow-Automated-GITHUB-FORK-1654149170272/code/job/pipeline.py
@@ -6,8 +6,10 @@ from job.udfs.UDFs import *
 from job.graph import *
 
 def pipeline(spark: SparkSession) -> None:
+    df_Source_1 = Source_1(spark)
     df_Source_0 = Source_0(spark)
     df_Limit_1 = Limit_1(spark, df_Source_0)
+    df_Script_1 = Script_1(spark)
 
 def main():
     Utils.initializeFromArgs(Utils.parseArgs())
