b523fd86f719ff6a48cd90b574ae7a943c7ca4f3
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-07-21 17:45:38 +0000

    simple commit

--- test/e2etestsmainforked5/pipelines/Workflow-Automated-GITHUB-FORK-1658425070768/code/job/pipeline.py
+++ test/e2etestsmainforked5/pipelines/Workflow-Automated-GITHUB-FORK-1658425070768/code/job/pipeline.py
@@ -9,6 +9,8 @@ from job.graph import *
 def pipeline(spark: SparkSession) -> None:
     df_customer_data_json = customer_data_json(spark)
     df_Limit_1 = Limit_1(spark, df_customer_data_json)
+    df_customer_data_json_1 = customer_data_json_1(spark)
+    df_Script_1 = Script_1(spark)
 
 def main():
     spark = SparkSession.builder\
