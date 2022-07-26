a79f280042dbe1d2e67d8978abc0f4cfd6f6216d
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-07-26 18:06:08 +0000

    Simple workflow Commit

---
+++ test/e2etestsmainforked5/pipelines/Workflow-Automated-GITHUB-FORK-1658858493909/code/job/pipeline.py
@@ -0,0 +1,28 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+from prophecy.utils import *
+from job.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_customer_data_json = customer_data_json(spark)
+    df_Limit_1 = Limit_1(spark, df_customer_data_json)
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "10750/pipelines/Workflow-Automated-GITHUB-FORK-1658858493909")
+    MetricsCollector.start(spark = spark, pipelineId = "10750/pipelines/Workflow-Automated-GITHUB-FORK-1658858493909")
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
