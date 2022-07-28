dfbf9e23561631f3043455b251c111783b523ebc
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-07-28 17:59:35 +0000

    Simple workflow Commit

---
+++ test/e2etestsmainforked5/pipelines/Workflow-Automated-GITHUB-FORK-1659031032957/code/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Workflow-Automated-GITHUB-FORK-1659031032957',
+    version = '1.0',
+    packages = find_packages(include = ('workflowautomatedgithubfork*', )),
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.1.14'],
+    entry_points = {
+'console_scripts' : [
+'main = workflowautomatedgithubfork.pipeline:main', ], },
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
